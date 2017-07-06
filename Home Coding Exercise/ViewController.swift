4b30bc7c2be05197f62d80a80433682346e7e81a
Povilas Martisius <povilas.martisius@presentconnection.nl>
2017-07-06 17:16:23 +0300

    Initial Commit

---
+++ Home Coding Exercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Home Coding Exercise
+//
+//  Created by Povilas Martisius on 7/6/17.
+//  Copyright © 2017 Povilas Martisius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
